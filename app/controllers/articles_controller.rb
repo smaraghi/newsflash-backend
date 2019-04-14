ba508f583c4bd83fffd7bf370441e580cdbcdfcf
Serven Maraghi <maraghiserven@gmail.com>
2019-04-14 16:48:12 -0400

    refactored controller and routes to include params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,36 +1,15 @@
 class ArticlesController < ApplicationController
   
   require 'open-uri'
+
+  URL1 = "https://newsapi.org/v2/top-headlines?country=us&category="
+  URL2 = "&pageSize=20&apiKey="
+
   KEY = '023a863893ee4bc9ac6f00d655b8a102'
 
-  def get_tech
-    url = "https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=20&apiKey=#{KEY}"
-    fetch(url)
-  end
 
-  def get_science
-    url = "https://newsapi.org/v2/top-headlines?country=us&category=science&pageSize=20&apiKey=#{KEY}"
-    fetch(url)
-  end
-
-  def get_entertainment
-    url = "https://newsapi.org/v2/top-headlines?country=us&category=entertainment&pageSize=20&apiKey=#{KEY}"
-    fetch(url)
-  end
-
-  def get_health
-    url = "https://newsapi.org/v2/top-headlines?country=us&category=health&pageSize=20&apiKey=#{KEY}"
-    fetch(url)
-  end
-
-  def get_business
-    url = "https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=20&apiKey=#{KEY}"
-    fetch(url)
-  end
-
-  def get_sports
-    url = "https://newsapi.org/v2/top-headlines?country=us&category=sports&pageSize=20&apiKey=#{KEY}"
-    fetch(url)
+  def get_fetch()
+    fetch(URL1 + params[:category] + URL2 + KEY) 
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get '/articles/sports', to: 'articles#get_sports'
-  get '/articles/tech', to: 'articles#get_tech'
-  get '/articles/science', to: 'articles#get_science'
-  get '/articles/entertainment', to: 'articles#get_entertainment'
-  get '/articles/health', to: 'articles#get_health'
-  get '/articles/business', to: 'articles#get_business'
+
+  get '/articles/:category', to: 'articles#get_fetch'
 
 end
 
