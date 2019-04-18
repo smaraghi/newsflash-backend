e4bc386d233ec3d0c3a8f3274fcef09261ad492b
Serven Maraghi <maraghiserven@gmail.com>
2019-04-18 12:27:38 -0400

    destructured grid

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -2,26 +2,27 @@ import React from 'react'
 import { Grid, Image, Segment } from 'semantic-ui-react'
 
 const Articles = props => {
+  const { title, description, urlToImage, url, author, publishedAt } = props.article
   return (
     <div>
       <div className="articleBox">
       <Grid columns='equal'>
         <Grid.Row stretched>
           <Grid.Column>
-            <Segment>Title: {props.article.title}</Segment>
-            <Segment>{props.article.description}</Segment>
+            <Segment>{title}</Segment>
+            <Segment>{description}</Segment>
           </Grid.Column>
           <Grid.Column width={6}>
             <Segment>
-              <Image src={props.article.urlToImage} alt=""/>
+              <Image src={urlToImage} alt=""/>
             </Segment>
             <Segment>
-              <p>{props.article.url}</p>
+              <p>{url}</p>
             </Segment>
           </Grid.Column>
           <Grid.Column>
-            <Segment>{props.article.author}</Segment>
-            <Segment>{props.article.publishedAt}</Segment>
+            <Segment>{author}</Segment>
+            <Segment>{publishedAt}</Segment>
           </Grid.Column>
         </Grid.Row>
       </Grid>
