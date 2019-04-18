896713ac7559fcf04a219a0a7b8b603252ccd7dc
Serven Maraghi <maraghiserven@gmail.com>
2019-04-17 23:08:31 -0400

    semantic grid initial addition

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,18 +1,31 @@
 import React from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
+import { Grid, Image, Segment } from 'semantic-ui-react'
 
 const Articles = props => {
   return (
     <div>
+      <div className="articleBox">
       <Grid columns='equal'>
         <Grid.Row stretched>
           <Grid.Column>
-            <Segment>{props.article.title}</Segment>
+            <Segment>Title: {props.article.title}</Segment>
             <Segment>{props.article.description}</Segment>
           </Grid.Column>
-
+          <Grid.Column width={6}>
+            <Segment>
+              <Image src={props.article.urlToImage} alt=""/>
+            </Segment>
+            <Segment>
+              <p>{props.article.url}</p>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>{props.article.author}</Segment>
+            <Segment>{props.article.publishedAt}</Segment>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
+      </div>
     </div>
   )
 }
