f7ac36c50de5118223102acffd12405d50ce128c
Serven Maraghi <maraghiserven@gmail.com>
2019-04-20 16:13:51 -0400

    date formmatted

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -7,6 +7,14 @@ const Articles = props => {
     return props.article.urlToImage ? props.article.urlToImage : require('../images/default.png')
   }
 
+  const formatDate = () => {
+    let date = props.article.publishedAt.split('T')[0]
+    let year = date.split('-')[0]
+    let day = date.split('-')[1]
+    let month = date.split('-')[2]
+    return `${day}-${month}-${year}`
+  }
+
   const { title, description, author, publishedAt } = props.article
   return (
     <Card>
@@ -14,11 +22,11 @@ const Articles = props => {
       <Card.Content>
         <Card.Header>{title}</Card.Header>
         <Card.Meta>{author}</Card.Meta>
-        <Card.Meta>{publishedAt}</Card.Meta>
+        <Card.Meta>{formatDate()}</Card.Meta>
         <Card.Description>{description}</Card.Description>
       </Card.Content>  
       <Modal trigger={<Button inverted color='blue'>Read More</Button>} centered={false}>
-        <ArticleModal article={props.article}/>
+       <ArticleModal article={props.article}/>
       </Modal>
     </Card>
   )
