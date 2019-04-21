d9e937b6b9f2e85fa81a0a60f3dc37ad7d79003d
Serven Maraghi <maraghiserven@gmail.com>
2019-04-21 18:36:32 -0400

    voting buttons set up including initial backend routing

--- client/src/components/ArticleModal.js
+++ client/src/components/ArticleModal.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import { Modal } from 'semantic-ui-react'
+import { Modal, Button, Icon, Label } from 'semantic-ui-react'
 
 const ArticleModal = props => {
 
   return (
     <Modal.Content>
-      <iframe height='500' width='700' src={props.article.url} title={props.article.title}></iframe>
+        <iframe height='500' width='850' src={props.article.url} title={props.article.title}></iframe>
+        <div className='voting-buttons-container'>
+          <Button as='div' labelPosition='right'>
+            <Button inverted color='twitter' icon>
+              <Icon name='heart' />
+              Like
+            </Button>
+            <Label basic pointing='left'>
+              500
+              {/* fetch call to backend for vote number potentially */}
+            </Label>
+          </Button>
+          <Button as='div' labelPosition='left'>
+            <Label basic pointing='right'>
+              500
+            </Label>
+            <Button inverted color='vk' icon>
+              <Icon name='x' />
+              Dislike
+            </Button>
+          </Button>
+        </div>
     </Modal.Content>
+   
   )
 }
 
