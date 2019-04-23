5cfc1f689df6b23dfd94219b14d557356a7e0526
Serven Maraghi <maraghiserven@gmail.com>
2019-04-22 20:42:31 -0400

    refactored homepage buttons css

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,64 +1,62 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Grid, Segment } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 class MainContainer extends Component {
   render() { 
     return ( 
       <div>
-        <Grid stackable columns={2} verticalAlign='middle'>
-          <Grid.Column >
-            
-              <Segment>
-                <Link to='/articles/technology'>
-                  <Button 
-                  circular color='violet' size='massive'
-                  >Tech
-                </Button>
-              </Link> 
-                
-              <Link to='/articles/sports'>
-                <Button 
-                  circular color='linkedin' size='massive' 
-                  >Sports
-                </Button>
-              </Link>
+        <div className='homepage-button-container'>
+          <Link to='/articles/technology'>
+            <Button 
+            id='homepage-button'
+            circular color='violet' size='massive'
+            >Tech
+          </Button>
+        </Link> 
+          
+        <Link to='/articles/sports'>
+          <Button 
+            id='homepage-button'
+            circular color='linkedin' size='massive' 
+            >Sports
+          </Button>
+        </Link>
 
-              <Link to='/articles/health'>
-                <Button 
-                  circular color='yellow' size='massive'
-                  >Health
-                </Button>
-              </Link>
+        <Link to='/articles/health'>
+          <Button 
+            id='homepage-button'
+            circular color='yellow' size='massive'
+            >Health
+          </Button>
+        </Link>
+      </div>
+          
+      <div className='homepage-button-container'>
+          <Link to='/articles/entertainment'>
+            <Button
+              id='homepage-button'
+              circular color='twitter' size='massive' 
+              >E.T.
+            </Button>
+          </Link>
 
-                </Segment>
-                <Segment>
+          <Link to='/articles/science'>
+            <Button
+              id='homepage-button'
+              circular color='teal' size='massive' 
+              >Science
+            </Button>
+          </Link>
 
-                <Link to='/articles/entertainment'>
-                  <Button
-                    circular color='twitter' size='massive' 
-                    >E.T.
-                  </Button>
-                </Link>
-
-                <Link to='/articles/science'>
-                  <Button
-                    circular color='teal' size='massive' 
-                    >Science
-                  </Button>
-                </Link>
-
-                <Link to='/articles/business'>
-                  <Button
-                    circular color='facebook' size='massive' 
-                    >Business
-                  </Button>
-                </Link>
-
-              </Segment>
-            
-          </Grid.Column>
-        </Grid>
+          <Link to='/articles/business'>
+            <Button
+              id='homepage-button'
+              circular color='facebook' size='massive' 
+              >Business
+            </Button>
+          </Link>
+        </div>
       </div>
      );
   }
